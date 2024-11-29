12a31f14690d2e43b537e07fa789b6914be0b7fc
DESKTOP-I5TD2H5\lsega <lsegars@gmail.com>
2024-11-29 16:14:03 -0300

    feat(nest) add env example

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const config = new DocumentBuilder()
     .setTitle('My first app')
     .setDescription('First config!!!')
@@ -14,6 +16,10 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, documentFactory);
 
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3010);
+
+  app.get(ConfigService);
+  console.log("Port: ",  process.env.PORT);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
