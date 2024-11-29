032b17172ead007132398d9b05e529fa761c9455
Leonardo <leonardo@finalis.com>
2024-11-29 14:55:30 -0300

    feat(nest): add validator class

--- src/task/entities/task.dto.ts
+++ src/task/entities/task.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from "@nestjs/swagger";
-import { Task } from "./task.entity";
+import { PartialType } from '@nestjs/swagger';
+import { Task } from './task.entity';
 
-export class taskDto extends PartialType(Task) {};
+export class taskDto extends PartialType(Task) {}
--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,37 +1,49 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { TaskService } from './task.service';
 import { taskDto } from './entities/task.dto';
 import { Task } from './entities/task.entity';
 
 @Controller('task')
 export class TaskController {
+  constructor(private readonly taskservice: TaskService) {}
 
-    constructor(private readonly taskservice: TaskService) {}
+  @Get()
+  async message(): Promise<string> {
+    return await this.taskservice.getMessage();
+  }
 
-    @Get()
-    async message(): Promise<string>{
-        return await this.taskservice.getMessage();
-    }
+  @Get('list')
+  async allTasks(): Promise<Task[]> {
+    return await this.taskservice.getAll();
+  }
 
-    @Get('list')
-    async allTasks(): Promise<Task[]>{
-        return await this.taskservice.getAll();
-    }
+  @Post()
+  @HttpCode(201)
+  async create(@Body() newtask: taskDto): Promise<Task> {
+    return await this.taskservice.create(newtask);
+  }
 
-    @Post()
-    @HttpCode(201)
-    async create(@Body() newtask: taskDto): Promise<Task> {
-        return await this.taskservice.create(newtask);
-    }
+  @Patch(':id')
+  @HttpCode(202)
+  async update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateTask: taskDto,
+  ): Promise<Task> {
+    return await this.taskservice.update(id, updateTask);
+  }
 
-    @Patch(':id')
-    @HttpCode(202)
-    async update(@Param('id') id: string, @Body() updateTask: taskDto): Promise<Task> {
-        return await this.taskservice.update(id, updateTask);
-    }
-
-    @Delete(':id')
-    async delete(@Param('id') id: string): Promise<Task> {
-        return this.taskservice.delete(id);
-    }    
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<Task> {
+    return this.taskservice.delete(id);
+  }
 }
